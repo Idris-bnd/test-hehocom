898d2f5e0eb702c204b4db700f704c9baaa41c57
idris-bnd <idris.pro83@yahoo.com>
2023-03-03 13:25:29 +0100

    exo 1 + création de la data pour l'exo 2 et début du HTML

---
+++ exo1/exo1.md
@@ -0,0 +1,33 @@
+## EXERCICE 1
+
+### Décris moi étape par étape cette fonction, les Valeurs des variables également par étape, Nomme moi ce type de fonction et ses avantages
+
+La fonction accepte trois paramètres optionnels qui sont initialisé comme ca de base:
+-$iteration : int de valeur 10
+-$resultat : array vide
+-$nb : int de valeur 0
+
+La fonction commence par copier le tableau $resultat dans une variable $final.
+
+Ensuite, elle teste si $iteration est supérieur à zéro et si $nb est égal à 0 ou 1. Si c'est le cas, elle ajoute $nb à $resultat.
+
+Sinon elle calcule la somme des deux derniers nombres de $resultat pour ajouter $nb.
+
+(dans ce cas là, $nb est égal à 0, donc la fonction ajoute $nb à $resultat)
+
+La fonction décrémente $iteration, incrémente $nb, et rappelle la fonction récursivement avec les nouveaux paramètres $iteration, $resultat et $nb.
+
+La fonction se rappelle elle-même jusqu'à ce que $iteration atteigne 0.
+  
+À ce moment-là, elle retourne le tableau complet des nombres
+
+-----------------
+
+Type de fonction: C'est une fonction récursive.
+définition google pour appuyer mes propos:
+Une fonction est dite récursive si, à l'intérieur de son corps, elle s'appelle elle-même avec une valeur de paramètre différent (sinon elle boucle).
+
+-----------------
+
+Ses avantages: Je dirais que c'est comme un while, on l'appel une fois puis elle 
+fais tout toute seule tant que le résultat est pas celui voulu.
---
+++ exo2/data.js
@@ -0,0 +1,59 @@
+import firstImg from './src/1.jpg'
+import secondImg from './src/2.jpg'
+import thirdImg from './src/3.jpg'
+import fourthImg from './src/4.jpg'
+import fifthImg from './src/5.jpg'
+import sixthImg from './src/6.jpg'
+import seventhImg from './src/7.jpg'
+import eighthImg from './src8.jpg'
+
+const data = [
+    {
+        img: firstImg,
+        title: "Cheuveux colorés",
+        text: "Entretenir, prévenir les cheveux secs et abimés avec la sélevtion jean marc joubert",
+        boldText: "Découvrez tous les soins et produits pour entretenir vos cheveux secs et abimés",
+    },
+    {
+        img: secondImg,
+        title: "Cheuveux secs et abimés",
+        text: "Entretenir, prévenir les cheveux secs et abimés avec la sélevtion jean marc joubert",
+        boldText: "Découvrez tous les soins et produits pour entretenir vos cheveux secs et abimés",
+    },
+    {
+        img: thirdImg,
+        title: "Cheuveux bouclés",
+        text: "Entretenir, prévenir les cheveux secs et abimés avec la sélevtion jean marc joubert",
+        boldText: "Découvrez tous les soins et produits pour entretenir vos cheveux secs et abimés",
+    },
+    {
+        img: fourthImg,
+        title: "Cheuveux ondulés",
+        text: "Entretenir, prévenir les cheveux secs et abimés avec la sélevtion jean marc joubert",
+        boldText: "Découvrez tous les soins et produits pour entretenir vos cheveux secs et abimés",
+    },
+    {
+        img: fifthImg,
+        title: "Cheuveux frisés",
+        text: "Entretenir, prévenir les cheveux secs et abimés avec la sélevtion jean marc joubert",
+        boldText: "Découvrez tous les soins et produits pour entretenir vos cheveux secs et abimés",
+    },
+    {
+        img: sixthImg,
+        title: "Cheuveux crépus",
+        text: "Entretenir, prévenir les cheveux secs et abimés avec la sélevtion jean marc joubert",
+        boldText: "Découvrez tous les soins et produits pour entretenir vos cheveux secs et abimés",
+    },
+    {
+        img: seventhImg,
+        title: "Cheuveux blonds",
+        text: "Entretenir, prévenir les cheveux secs et abimés avec la sélevtion jean marc joubert",
+        boldText: "Découvrez tous les soins et produits pour entretenir vos cheveux secs et abimés",
+    },
+    {
+        img: eighthImg,
+        title: "Cheuveux raides",
+        text: "Entretenir, prévenir les cheveux secs et abimés avec la sélevtion jean marc joubert",
+        boldText: "Découvrez tous les soins et produits pour entretenir vos cheveux secs et abimés",
+    },
+]
